bfa4726a5213173fd34a1500a87e79d450812911
Dennis Ren <rmr1012@gmail.com>
2018-10-13 05:04:55 -0700

    huhan stablized

--- sand.py
+++ sand.py
@@ -1,17 +1,13 @@
-import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+from huHann import huHann
 
-img = cv2.imread('IMGP2395.JPG',0)
+img = cv2.imread('IMGP2395.JPG')
+img=img[:,:,2].astype(int)
 
-dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
-dft_shift = np.fft.fftshift(dft)
+blurmap=huHann(img)
 
-magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
-
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.imshow(blurmap)
+plt.colorbar()
 plt.show()
