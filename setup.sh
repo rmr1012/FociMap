2140d370d1933754626717a8610fd9cb55f79e53
Sumeet Bansal <sumeetbansal@gmail.com>
2018-10-14 05:04:18 +0000

    added setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#1/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y python3 python3-dev python3-setuptools
+sudo apt-get update && sudo apt-get install python3-pip
+sudo apt-get install -y libsm6 libxext6 libxrender-dev
+git clone https://github.com/rmr1012/FociMap
