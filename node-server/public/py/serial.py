032bb50d2ab02ac33dcbca4bbced1760c1837aec
root <root@instance-1.c.sdhacks18.internal>
2018-10-14 15:01:23 +0000

    cleanup added to python

--- node-server/public/py/serial.py
+++ node-server/public/py/serial.py
@@ -1,4 +1,4 @@
-
+import copy
 import matplotlib
 from matplotlib import pyplot as plt
 from ren import renAlgo
@@ -22,7 +22,7 @@ def crunchSerial(inSer,outfile):
     print("outfile",outfile)
 #    jsonData=json.loads(open(inJson,"r").read())
 #    jsonData=jsonData["fileset"]
-    focal=[0.5, 0.6, .7, .75, .8, .85, .9, .95]
+    focal=[0.6, 0.65, 0.70, 0.72, 0.75, 0.78, 0.8, 0.95]
     filepaths=["public/images/"+str(inSer)+"_"+str(ind) for ind in range(8) ]
     confMaps=[]
     for ohPath in filepaths:
@@ -38,15 +38,46 @@ def crunchSerial(inSer,outfile):
     depth_1d=np.array(list(map(lambda x: focal[x],max_idx)))
     oneDepth=depth_1d.reshape(semiFlatConf.shape[1],semiFlatConf.shape[2])
 
-    normDep=255*(oneDepth-min(focal))/(max(focal)-min(focal))
-    result=neighbourReplace(normDep,100)    
+    normalize=255*(oneDepth-min(focal))/(max(focal)-min(focal))
+    mu=  cv2.blur(normalize, (100,100))
+    mu2= cv2.blur(np.multiply(normalize,normalize), (100,100));
+    sigma=cv2.sqrt(np.abs(mu2 - np.multiply(mu,mu)))
+    #bisigma=copy.copy(sigma)
+    tresh=sigma.max()*0.45
+    fatblur=cv2.blur(normalize, (500,500))
+    normalize[np.where(sigma>tresh)]= fatblur[np.where(sigma>tresh)]
+
+    normalize=255-normalize
+    mu=  cv2.blur(normalize, (50,50))
+    mu2= cv2.blur(np.multiply(normalize,normalize), (50,50));
+    sigma=cv2.sqrt(np.abs(mu2 - np.multiply(mu,mu)))
+    #bisigma=copy.copy(sigma)
+    tresh=sigma.max()*0.45
+    fatblur=cv2.blur(normalize, (500,500))
+    normalize[np.where(sigma>tresh)]= fatblur[np.where(sigma>tresh)]
+
+    normalize=255-normalize
+    eqed=cv2.equalizeHist(normalize.astype(np.uint8))
+
+    result=eqed#cv2.blur(eqed,(25,25))
+
+#    normDep=255*(oneDepth-min(focal))/(max(focal)-min(focal))
+ #   result=neighbourReplace(normDep,100)    
 # and renormalize
 
     print("finished Ren Algo, saving")
     # plt.imshow(smoothDepth,cmap='gray')
     # plt.colorbar()
     # pickle.dump(smoothDepth, open( "smoothDepth.p", "wb" ) )
-
+    cmd=("cp public/images/"+str(inSer)+"_6.jpg public/images/"+str(inSer)+".jpg")
+    print(cmd)
+    os.system(cmd)
+    for ohPath in filepaths:
+        try:
+            os.remove(ohPath+".jpg.p")
+            os.remove(ohPath+".jpg")
+        except:
+            pass
     plt.imsave(outfile,result,cmap='gray')
 
 
