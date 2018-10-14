e93d2097b7c2d9d7c002e2832931ef6a4249cdf8
Sumeet Bansal <sumeetbansal@gmail.com>
2018-10-13 17:38:01 -0700

    templatized render page with route parameters

--- node-server/routes/index.js
+++ node-server/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.render('index');
+router.get('/:render', function(req, res, next) {
+	res.render('index', { "render" : req.params.render });
 });
 
 module.exports = router;
