acfeec2c2adcfef410b87e5ee6dead9cd50fdd52
Sumeet Bansal <sumeetbansal@gmail.com>
2018-10-14 10:12:45 +0000

    3:12 am fuck me

--- node-server/public/py/serial.py
+++ node-server/public/py/serial.py
@@ -16,17 +16,19 @@ def neighbourReplace(src,size):
     Rf=scipy.ndimage.maximum_filter(src,size=size)
     src[np.where(src<=0.49)]= Rf[np.where(src<=0.49)]
     return src
-def crunchSerial(inJson,outfile):
+def crunchSerial(inSer,outfile):
     print("serial processing")
+    print("inSer",inSer)
+    print("outfile",outfile)
     jsonData=json.loads(open(inJson,"r").read())
     jsonData=jsonData["fileset"]
-    focal=[]
+    focal=[0.4 0.55 .7 .72 .75 .77 .8 .85]
+    filepaths=["public/images/"+str(inSer)+"_"+str(ind+1) for ind in range(8) ]
     confMaps=[]
-    for item in jsonData:
-        focal.append(item["focal"])
-        confMaps.append(pickle.load(open(item["filename"]+".p","rb")))
+     for ohPath in filepaths:
+         confMaps.append(pickle.load(open(ohPath+".p","rb")))
 
-#    flatConf=np.array(confMaps)
+    flatConf=np.array(confMaps)
 
     semiFlatConf=np.array(confMaps)
     flatConf=semiFlatConf.reshape(semiFlatConf.shape[0],-1)
@@ -48,7 +50,7 @@ def crunchSerial(inJson,outfile):
 
 
 if __name__=="__main__":
-    inJson=sys.argv[1]
+    inSer=sys.argv[1]
     outFile=sys.argv[2]
-
-    crunchSerial(inJson,outFile)
+    
+    crunchSerial(inSer,outFile)
--- node-server/routes/index.js
+++ node-server/routes/index.js
@@ -1,5 +1,5 @@
-"use strict";
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 router.get('/:render', function(req, res, next) {
@@ -7,41 +7,50 @@ router.get('/:render', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res, next) {
+	console.log("uploading " + req.body.id);
 	let buf = Buffer.from(req.body.data, 'base64');
-	let file = req.body.id + '.jpg'
+	let file = req.body.id + '_' + req.body.index + '.jpg'
 	let path = './public/images/' + file;
 	fs.writeFile(path, buf, function(err) {
 		if (err) {
-			return res.send({ "status" : "failed" });
+			console.log("failed to upload " + file);
+			return res.end("failed");
 		}
 		let spawn = require('child_process').spawn;
-		let pyproc = spawn('python', ['./public/py/parallel.py', path]);
+		let pyproc = spawn('python3', ['./public/py/parallel.py', path]);
 		pyproc.stderr.on('data', function(data) {
-			return res.send({ "status" : "failed" });
+			console.log(data.toString());
+			return res.end("failed");
 		});
 		pyproc.stdout.on('data', function(data) {
 			console.log(data.toString('utf8'));
 		});
 		pyproc.stdout.on('end', function(data) {
-			return res.send({ "status" : 200 });
+			return res.end("success");
 		});
 	});
 });
 
 router.post('/url', function(req, res, next) {
+	console.log(req.body);
 	let spawn = require('child_process').spawn;
 	let jsonfile = './public/json/' + req.body.id + '.json';
 	let depthfile = './public/images' + req.body.id + '-depth.jpg'
-	fs.writeFile(jsonfile, req.body, function(err) {
-		let pyproc = spawn('python', ['./public/py/serial.py', jsonfile, depthfile]);
+	let data = JSON.stringify(req.body);
+	fs.writeFile(jsonfile, data, function(err) {
+		if (err) {
+			console.log("failed to write " + jsonfile);
+			return res.end("failed");
+		}
+		let pyproc = spawn('python3', ['./public/py/serial.py', "8032068921399739550", depthfile]);
 		pyproc.stderr.on('data', function(data) {
-			return res.send({ "status" : "failed" });
+			return res.end("failed");
 		});
 		pyproc.stdout.on('data', function(data) {
 			console.log(data.toString('utf8'));
 		});
 		pyproc.stdout.on('end', function(data) {
-			return res.send({ "status" : 200 });
+			return res.end("success");
 		});
 	});
 });
